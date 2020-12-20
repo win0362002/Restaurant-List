7f55f3206cdb610a048af4a433aa01bfdf97955e
Mike <win0362002@hotmail.com>
2020-12-20 22:21:59 +0800

    Add edit restaurant function

--- app.js
+++ app.js
@@ -66,9 +66,42 @@ app.post('/restaurants', (req, res) => {
 //Show restaurant info
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
-  Restaurant.findById(id)
+  return Restaurant.findById(id)
     .lean()
     .then((restaurant) => res.render('show', { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+//Edit restaurant info
+//Show restaurant info
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const category = req.body.category
+  const location = req.body.location
+  const phone = req.body.phone
+  const rating = req.body.rating
+  const description = req.body.description
+
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch((error) => console.log(error))
 })
 
