7a22cf29762341030a363ba208e7bcde2ad058d6
Mike <win0362002@hotmail.com>
2020-12-20 23:11:19 +0800

    Add delete restaurant function

--- app.js
+++ app.js
@@ -105,6 +105,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+// Delete restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 //Implement search bar
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
